5df3729d0ca6f541a581c954139196cff802545d
Jenny Kim <kimj1905@gmail.com>
2021-11-30 21:49:34 -0500

    redo jwt setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,30 @@
 class ApplicationController < ActionController::API
-    before_action :authenticate
+    # before_action :authenticate
 
-    def authenticate
-        if request.headers["Authorization"]
-            begin
-                auth_header = request.headers["Authorization"]
-                decoded_token = JWT.decode(token, secret)
-                payload = decoded_token.first
-                @user = User.find(payload["user_id"])
-            rescue => exception
-                render json: {message: "Error: invalid token"}, status: :forbidden
-            end
-        else
-            render json: {message: "No authorization header sent."}, status: :forbidden
-        end
-    end
+    # def authenticate
+    #     if request.headers["Authorization"]
+    #         begin
+    #             auth_header = request.headers["Authorization"]
+    #             decoded_token = JWT.decode(token, secret)
+    #             payload = decoded_token.first
+    #             @user = User.find(payload["user_id"])
+    #         rescue => exception
+    #             render json: {message: "Error: invalid token"}, status: :forbidden
+    #         end
+    #     else
+    #         render json: {message: "No authorization header sent."}, status: :forbidden
+    #     end
+    # end
 
-    def token
-        auth_header.split(" ")[1]
-    end
+    # def token
+    #     auth_header.split(" ")[1]
+    # end
 
-    def secret
-        secret = ENV["SECRET_KEY_BASE"] || Rails.application.secrets.secret_key_base
-    end
+    # def secret
+    #     secret = ENV["SECRET_KEY_BASE"] || Rails.application.secrets.secret_key_base
+    # end
 
-    def create_token(payload)
-        JWT.encode(payload, secret)
-    end
+    # def create_token(payload)
+    #     JWT.encode(payload, secret)
+    # end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
     def create
         user = User.new(user_params)
+
         if user.save
+            # payload = {user_id: user.id}
+            # token = create_token(payload)
             render json: user
         else
             render json: {error: "User could not be created. Please try again."}
