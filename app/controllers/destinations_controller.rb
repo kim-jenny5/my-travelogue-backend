944f7d684b7f664c47fd85e45a247e935d1b863c
Jenny Kim <kimj1905@gmail.com>
2021-12-01 12:26:11 -0500

    save

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -3,4 +3,9 @@ class DestinationsController < ApplicationController
         destinations = Destination.all
         render json: destinations
     end
+
+    private
+    def destination_params
+        params.require(:destination).permit(:id, :trip_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :destinations
   resources :trips
   resources :users
+
+  # post "/login", to: "sessions#create"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
