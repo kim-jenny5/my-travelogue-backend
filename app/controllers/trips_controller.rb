557ae6eab4a653f0ff48b2ed9e56a888e9d3ce58
Jenny Kim <kimj1905@gmail.com>
2021-12-01 12:20:16 -0500

    added trip params and edited user serializer

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,4 +3,10 @@ class TripsController < ApplicationController
         trips = Trip.all
         render json: trips
     end
+
+    private
+
+    def trip_params
+        params.require(:trip).permit(:id, :name, :location, :start_date, :end_date, :user_id)
+    end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :password_digest
+  attributes :id, :created_at, :first_name, :last_name, :email, :password_digest
+
+  # def joined
+    
+  # end
 end
