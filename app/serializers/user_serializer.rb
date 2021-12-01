20226c15e799c1bbc61337043fb8df91728ddbb0
Jenny Kim <kimj1905@gmail.com>
2021-12-01 13:29:01 -0500

    edited user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  # attributes :id, :created_at, :first_name, :last_name, :email, :password_digest
   attributes :id, :joined, :first_name, :last_name, :email, :password_digest
 
   def joined
-    users = User.all
-    users.each do |user|
-      date = user.created_at.strftime("%B %e, %Y")
-      return date
-    end
+    object.created_at.strftime("%B %e, %Y")
   end
 end
