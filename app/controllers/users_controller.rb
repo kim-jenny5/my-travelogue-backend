cae2e714120e06f73958de374ca421074d9948e6
Jenny Kim <kimj1905@gmail.com>
2021-11-30 21:41:02 -0500

    created barebones user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: {error: "User could not be created. Please try again."}
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+jenny = User.create(first_name: "Jenny", last_name: "Kim", email: "jennykimdev@gmail.com", password: "1234")
+# disney = Trip.create(name: "Disney World! ✨", location: "Orlando, FL", start_date: )
+# t.string :name
+# t.string :location
+# t.datetime :start_date
+# t.datetime :end_date
+# t.belongs_to :user, null: false, foreign_key: true
