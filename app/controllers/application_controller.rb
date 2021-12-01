b35ef434500354bc98fc9c665df502362b5197b2
Jenny Kim <kimj1905@gmail.com>
2021-12-01 12:02:08 -0500

    added jwt token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,42 @@
 class ApplicationController < ActionController::API
-    # def jwt_key
-    #     ENV["SESSION_SECRET"]
-    # end
+    before_action :authorized
 
-    # def issue_token(user)
-    #     JWT.encode({user_id: user.id}, jwt_key, "HS256")
-    # end
+    def jwt_key
+        # ENV["SESSION_SECRET"]
+        Rails.application.credentials.jwt_key
+    end
+
+    def issue_token(user)
+        JWT.encode({user_id: user.id}, jwt_key, "HS256")
+    end
+
+    def decoded_token
+        begin
+            JWT.decode(token, jwt_key, true, { :algorithm => 'HS256' })
+        rescue => exception
+            [{error: "Invalid Token"}]
+        end    
+    end
+
+    def authorized
+        render json: {message: "Please log in."} unless logged_in?
+    end
+
+    def token
+        request.headers["Authorization"]
+    end
+
+    def user_id
+        decoded_token.first["user_id"]
+    end
+
+    def current_user
+        @user ||= User.find_by(id: user_id)
+    end
+
+    def logged_in?
+        !!current_user
+    end
 
     # before_action :authenticate
 
