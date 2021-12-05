8b239b1e8113ab4417a6d5d941867260f7e08c10
Jenny Kim <kimj1905@gmail.com>
2021-12-04 23:04:53 -0500

    uncommented out trip index to be able to fetch from frontend

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   get "/authorized", to: "sessions#show"
   post "/newtrip", to: "trips#create"
-  get "/dashboard", to: "users#show"
+  # get "/dashboard", to: "users#show"
+  get  "/dashboard", to: "trips#index"
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
