4145d63b38ad3d43a7c125ffca3084a9a92f7036
Jenny Kim <kimj1905@gmail.com>
2021-12-05 14:30:04 -0500

    changed back datetime to date for trips for the last time

--- db/migrate/20211205192800_changing_trips_table.rb
+++ db/migrate/20211205192800_changing_trips_table.rb
@@ -2,5 +2,7 @@ class ChangingTripsTable < ActiveRecord::Migration[6.1]
   def change
     remove_column :trips, :location, :string
     rename_column :trips, :name, :trip_name
+    change_column :trips, :start_date, :date
+    change_column :trips, :end_date, :date
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_182249) do
+ActiveRecord::Schema.define(version: 2021_12_05_192800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 2021_12_05_182249) do
 
   create_table "trips", force: :cascade do |t|
     t.string "trip_name"
-    t.datetime "start_date"
-    t.datetime "end_date"
+    t.date "start_date"
+    t.date "end_date"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
