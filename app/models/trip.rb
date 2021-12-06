f353447225fbd830bbe8f71a77e906778cedb19c
Jenny Kim <kimj1905@gmail.com>
2021-12-05 20:36:35 -0500

    validated start and end date for trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,9 @@
 class Trip < ApplicationRecord
   belongs_to :user
   has_many :destinations
+
+  # VALIDATE START_DATE AND END_DATE
+  validates :start_date, presence: {message: "There must be a start date."}
+  validates :end_date, presence: {message: "There must be an end date."}
+
 end
