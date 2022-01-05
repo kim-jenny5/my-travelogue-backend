002fb2a1020da8f1087250045279fb544030c9d1
Jenny Kim <kimj1905@gmail.com>
2022-01-04 20:20:23 -0500

    edited cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,4 +31,11 @@ class ApplicationController < ActionController::API
     def logged_in?
         !!current_user
     end
+
+    # def cors_set_access_control_headers
+    #     headers['Access-Control-Allow-Origin'] = '*'
+    #     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, PATCH, OPTIONS'
+    #     headers['Access-Control-Request-Method'] = '*'
+    #     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    # end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://my-travelogue.netlify.app/'
+    origins 'https://my-travelogue.netlify.app'
 
     resource '*',
       headers: :any,
